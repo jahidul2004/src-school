b91d358ecbb805bfde7bcfaa2066f8000ed8663b
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-29 21:14:14 +0600

    About us page added.

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../home/home/Home";
 import Login from "../login/Login";
+import About from "../pages/aboutUs/About";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path:"/login",
                 element:<Login></Login>
+            },
+            {
+                path:"about",
+                element: <About></About>
             }
         ]
     },
